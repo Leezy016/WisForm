cde66e72e33525267d08614847ea82acec7abce7
WangJia <3211868668@qq.com>
2024-05-20 17:34:56 +0800

    delete .git

---
+++ frontEnd/src/components/UserDashboard.vue
@@ -0,0 +1,25 @@
+<!-- src/components/UserDashboard.vue -->
+
+<template>
+    <div class="user-dashboard">
+        <h2>欢迎, 普通用户</h2>
+        <DynamicForm />
+    </div>
+</template>
+
+<script>
+    import DynamicForm from '../components/DynamicForm.vue';
+
+    export default {
+        components: {
+            DynamicForm
+        }
+    };
+</script>
+
+<style scoped>
+    .user-dashboard {
+        max-width: 600px;
+        margin: auto;
+    }
+</style>
