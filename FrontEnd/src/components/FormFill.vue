228a525f7b02525d8265af62d096e1a738ef7ecc
LiSijia <492990742@qq.com>
2024-05-31 19:33:02 +0800

    add-titleList

--- FrontEnd/src/components/FormFill.vue
+++ FrontEnd/src/components/FormFill.vue
@@ -1,16 +1,64 @@
-<template>
-  <div class="form-fill">
-      <NavBar/>
-  </div>
+<template>  
+  <div class="form-fill">  
+    <NavBar/>
+    <table class="table">  
+      <thead>  
+        <tr><th>请选择需要填写的表单</th></tr>  
+      </thead>  
+      <tbody>  
+        <tr v-for="(title,index) in titles" :key="index" @click="goToDetail(title)">  
+          <td>{{ title }}</td>  
+        </tr>  
+      </tbody>  
+    </table> 
+    <p v-if="getErrorMessage" class="errorMessage">{{ getErrorMessage }}</p> 
+  </div>  
 </template>
 
 <script>
-  import NavBar from '../components/NavBar.vue';
-  export default {
-      components: {
+import NavBar from '../components/NavBar.vue'; 
+import axios from 'axios';
+export default {  
+  data() {  
+    return {  
+      titles: ['titile1'],
+      getErrorMessage:'' 
+    };  
+  },  
+  components: {
           NavBar,
-      }
-  };
+      },
+  methods: {  
+    getTitles() {   
+      axios.post('http://localhost:8080/api/register', {  
+        })  
+        .then(response => {  
+          if (response.data.success) { 
+            //console.log('表单提交成功'); 
+            this.titles=response.data.titles
+          }
+          else {  
+            this.getErrorMessage = response.data.message || '表单列表获取失败，请稍后再试';  
+          }
+         })  
+        .catch(error => {  
+          if (error.response) {  
+            // 后端返回的错误信息
+            this.getErrorMessage = error.response.data;   
+          }
+          else {
+            this.getErrorMessage = '表单提交失败，请稍后再试';
+          }
+        });  
+    },  
+    goToDetail(title) {  
+      this.$router.push({ name: 'FormFillDetail', params: { title: title} });  
+    }  
+  },  
+  created() {  
+    this.getTitles(); // 在组件创建时获取数据  
+  }  
+};  
 </script>
 
 <style>  
@@ -19,8 +67,8 @@
   flex-direction: row;  
 }  
   
-.content {  
-  margin-left: 200px; /* 与侧边栏的宽度相同，以确保内容不会与侧边栏重叠 */  
-  /* 其他样式... */  
+.form-fill{  
+  max-width: 400px;
+  margin: auto;
 }  
 </style>
